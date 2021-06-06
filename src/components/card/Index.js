8f8229589e8c2606801f21a92b86174e75972500
muhsin sutanto <sutantomuhsin@gmail.com>
2021-06-06 11:43:13 +0700

    update card component

--- src/components/card/Index.js
+++ src/components/card/Index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './card.css'
 
 const Card = () => {
-    return ( <di>
-        hai
-    </di> );
+    return (  
+        <div className='card'>
+            <div className='card-left'>
+                <div className='card-header'>
+                    <h2>USD</h2>
+                    <h2>10.0000</h2>
+                </div>
+                <h3>USD - United States Dolars</h3>
+                <h4>1 USD = IDR 14.410.45</h4>
+            </div>
+            <div className='card-right'>
+                <button>(-)</button>
+            </div>
+        </div> 
+        );
 }
  
 export default Card;
--- src/pages/homePage/Index.js
+++ src/pages/homePage/Index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Card from '../../components/card/Index';
+
 import './homeStyle.css'
 
 const Home = () => {
@@ -13,19 +15,7 @@ const Home = () => {
             </div>
         </div>
         <div className='card-container'>
-            <div className='card'>
-                <div className='card-left'>
-                    <div className='card-header'>
-                        <h2>USD</h2>
-                        <h2>10.0000</h2>
-                    </div>
-                    <h3>USD - United States Dolars</h3>
-                    <h4>1 USD = IDR 14.410.45</h4>
-                </div>
-                <div className='card-right'>
-                    <button>(-)</button>
-                </div>
-            </div>
+            <Card/>
         </div>
     </div> );
 }
